060786308d58c7e4d547f3870a237e81dda08b90
Martijn Stolk <martijn.stolk@gmail.com>
2014-12-30 09:10:44 +0100

    Add a small testapp for local testing on devstorage

---
+++ Ms.Azure.Logging.TestApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using log4net;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
+using Microsoft.WindowsAzure.Storage;
+using Ms.Azure.Logging.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ms.Azure.Logging.TestApp
+{
+    class Program
+    {
+        private static readonly ILog Logger = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            LoggingHelper.InitializeAzureTableLogging(CloudStorageAccount.DevelopmentStorageAccount);
+            (LogManager.GetRepository() as Hierarchy).Root.AddAppender(new log4net.Appender.ConsoleAppender { Layout = new PatternLayout("%date %-5level [%-3thread] %logger - %message%newline") });
+            Logger.Info("Logging initialized");
+            for (int i = 1; i <= 10; i++)
+            {
+                Logger.Info("Message " + i + "/" + 10);
+                Thread.Sleep(1000);
+            }
+            Logger.Info("Done");
+            LoggingHelper.FlushAppenders();
+        }
+    }
+}
